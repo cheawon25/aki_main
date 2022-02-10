291a8eeeaa1abd2def0d52bce699983303524e95
cheawon25 <99226711+cheawon25@users.noreply.github.com>
2022-02-10 11:18:45 +0900

    Delete script directory

--- script/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $(".mainmenu>li").hover(function(){
-    $(this).children("#sub").stop().slideDown();
-  },
-  function(){
-    $(this).children("#sub").stop().slideUp();
-  });
-});
